de81e9ac2439b4894f9faaf79e651f9f7cf557e5
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-02-16 14:42:09 +0300

    initial commit

---
+++ s18d4/src/test/java/com/example/s18d4/S18d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s18d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
